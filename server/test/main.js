85a1181c9ddff5c139efdd7fbfdbbadd3970909e
Mohammed EL HADROUBI <med.elhadroubi@gmail.com>
2017-02-22 20:32:08 +0000

    Timeout tests after n  minutes

--- server/test/main.js
+++ server/test/main.js
@@ -5,6 +5,13 @@ const path=require('path');
 
 //TODO Handle missing argument 'suite'
 const suite=Optimist.argv.suite;
+//TODO Dirty hack to be cleaned up
+const MINUTES=60*1000;
+const DEFAULT_TIMEOUT=2*MINUTES;
+const exit=()=>{
+  process.exit(electronProcess.status);
+};
+setTimeout(exit, DEFAULT_TIMEOUT);
 const electronProcess=ChildProcess.spawnSync(electron, [path.join(__dirname, suite, 'main.js')], {stdio:'inherit'});
-process.exit(electronProcess.status);
+exit();
 
